907850c8ae0b2ff21bb353ac21abf996836700ad
somin <qkrthals0524@naver.com>
2021-05-08 21:51:18 +0900

    Delete file

--- server/dailycat/users/models.py
+++ server/dailycat/users/models.py
@@ -8,14 +8,6 @@ class User(AbstractUser):
     User 모델
 
     '''
-    # first_name = None
-    # last_name = None
-    # username = None
-    # email = models.EmailField(unique=True)
-    # password = models.CharField(max_length=128)
-
-    # USERNAME_FIELD = 'email'
-    # REQUIRED_FIELDS = []
     liked_cats = models.ManyToManyField(Cat, related_name='user_set')
     liked_titles = models.ManyToManyField(Title, related_name='user_set')
 
--- server/dailycat/users/urls.py
+++ server/dailycat/users/urls.py
@@ -8,14 +8,5 @@ urlpatterns = [
     # path('login/', LoginView.as_view(), name='login'),
     # path('signup/', RegisterView.as_view(), name='signup'),
     # path('logout/', rest_auth.views.LogoutView.as_view(), name='logout'),
-    # path('account-confirm-email/',
-    #      VerifyEmailView.as_view(), name='account_confirm_email'),
-    # # path('signup', SignupView.as_view(), name='signup')
-    # path('rest-auth/', include('rest_auth.urls')),
-    # path('signup/', include('rest_auth.registration.urls'))
-    # user/registration/ => 회원가입
-    #username, password1,password2,email
-    # user/login/ =>로그인
-    # username,email,password
-    # user/logout/
+
 ]
