ccf007b88cdd85fd7c8f4bcade3f470ca52db108
Jingyu-Lim <jg@brskorea.co.kr>
2021-05-15 03:32:55 -0400

    메인화면 구성 진행중

--- client/component/Content/Content.js
+++ client/component/Content/Content.js
@@ -2,33 +2,48 @@ import React, { useState } from "react";
 import styles from './Content.module.css';
 import { Toggle } from "../../component";
 
-const Content = () => {
+const Content = (props) => {
   const [heart, heartOn] = useState(false);
-  const [bug, bugOn] = useState(false);
   const [network, networkOn] = useState(false);
   
   return (
     <div className={styles.content_wrap}>
-      <div className={styles.photo}></div>
+      <div className={styles.photo}>
+        <img src={props.main_image} alt="content_image"/>
+      </div>
       <div className={styles.desc}>
-        <Toggle
-            isOn={bug}
-            onClickHandler={()=>bugOn(!bug)}
-            icon="im im-bug"
-            activeColor="orange"
-        />
-        <Toggle
-            isOn={heart}
-            onClickHandler={()=>heartOn(!heart)}
-            icon="im im-heart"
-            activeColor="red"
-        />
-        <Toggle
-            isOn={network}
-            onClickHandler={()=>networkOn(!network)}
-            icon="im im-network"
-            activeColor="blue"
-        />
+        <div className={styles.icon_bar}>
+          <Toggle
+              isOn={true}
+              onClickHandler={null}
+              icon="im im-info"
+              activeColor="orange"
+          />
+          <Toggle
+              isOn={heart}
+              onClickHandler={()=>heartOn(!heart)}
+              icon="im im-heart"
+              activeColor="red"
+          />
+          <Toggle
+              isOn={network}
+              onClickHandler={()=>networkOn(!network)}
+              icon="im im-megaphone"
+              activeColor="blue"
+          />
+        </div>
+        <div className={styles.title_wrap}>
+          <div className={styles.title_input}>
+            <div>
+              <input type="text" />
+            </div>
+            <div>
+              <button>
+                <i className="im im-upload"/>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
